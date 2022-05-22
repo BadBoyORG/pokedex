a9ef1963646cdbb267a4442aab0c29c524ee92ae
Brian Santos <sbriansantos2002@gmail.com>
2022-05-22 04:00:33 -0300

    home responsive desktop and 4k version

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -7,14 +7,45 @@ export const Container = styled.div`
   display: flex;
   flex-direction: row;
   padding: 10rem 0 5rem 9rem;
+  margin: 0 auto;
   align-items: center;
   justify-content: space-between;
+
+  //desktop responsive
+  @media screen and (max-width: 1160px) {
+    padding: 0 0 0 3rem;
+  }
+
+  @media screen and (max-width: 1350px) {
+    padding: 0 0 0 6rem;
+  }
+
+  @media screen and (min-width: 2000px) {
+    padding: 10rem 0 5rem 12rem;
+  }
 `;
 
 export const ContainerColumn = styled.div`
   display: flex;
   flex-direction: column;
-  margin-right: 12.5rem;
+  margin-right: 10rem;
+
+  //desktop responsive
+  @media screen and (max-width: 1350px) {
+    margin-right: 5rem;
+  }
+
+  @media screen and (min-width: 1850px) {
+    margin-right: 20rem;
+  }
+
+  @media screen and (min-width: 2000px) {
+    margin-right: 20rem;
+  }
+
+  @media screen and (min-width: 2560px) {
+    margin-right: 30rem;
+  }
 `;
 
 export const Title = styled.h1`
@@ -29,6 +60,19 @@ export const Title = styled.h1`
   span {
     font-weight: 700;
   }
+
+  //desktop responsive
+  @media screen and (min-width: 1850px) {
+    margin-bottom: 2.8rem;
+  }
+
+  @media screen and (min-width: 2000px) {
+    margin-bottom: 3.5rem;
+  }
+
+  @media screen and (min-width: 2560px) {
+    margin-bottom: 4rem;
+  }
 `;
 
 export const Paragraph = styled.p`
@@ -38,6 +82,19 @@ export const Paragraph = styled.p`
   line-height: 37.41px;
   color: ${({theme}) => theme.colors.dark};
   margin-bottom: 2.5rem;
+
+  //desktop responsive
+  @media screen and (min-width: 1850px) {
+    margin-bottom: 2.8rem;
+  }
+
+  @media screen and (min-width: 2000px) {
+    margin-bottom: 3.5rem;
+  }
+
+  @media screen and (min-width: 2560px) {
+    margin-bottom: 4rem;
+  }
 `;
 
 export const Button = styled.button`
@@ -67,4 +124,21 @@ export const Button = styled.button`
 
 export const PikachuImg = styled.img`
   width: 40rem;
+
+  //desktop responsive
+  @media screen and (max-width: 1160px) {
+    width: 35rem;
+  }
+
+  @media screen and (min-width: 1850px) {
+    width: 120rem;
+  }
+
+  @media screen and (min-width: 2000px) {
+    width: 120rem;
+  }
+
+  @media screen and (min-width: 2560px) {
+    width: 120rem;
+  }
 `;
